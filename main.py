76df582b586f74ffcfaf0d78286fc8ffcfc79fdc
Daniel Mikkers <162559199+DanielMikkers@users.noreply.github.com>
2024-05-08 14:43:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import numpy as np
 
-class Lattice:
-  def __init__(self, n_sites):
-    self.n_sites = n_sites
+choice_dict = {'binary': np.array([-1,1])}
 
+class RandomLattice:
   def perf_square(self, n_sites):
     sqrt_n = np.sqrt(n_sites)
     if not isinstance(sqrt_n, int):
@@ -12,7 +11,10 @@ class Lattice:
       print("n_sites rounded to integer: {%d}".format(sqrt_n)
     return sqrt_n
 
-  def random_lattice(self, n_sites):
-    n_sites = perf_square(n_sites)
+  def random_lattice(self, n_sites, model='binary', prob=np.array([0.5,0.5])):
+    sqrt_size = perf_square(n_sites)
+    size = (sqrt_size,sqrt_size)
+    choice = choice_dict[model]
+    lattice = np.random.uniform(choice, size=size), p=prob)
 
-    
+    return lattice
